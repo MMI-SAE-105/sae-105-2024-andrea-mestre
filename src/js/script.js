969a2dfc9707217c83c84779ffd0bb61a04faea9
andrea-mestre <andrea.mestre@edu.univ-fcomte.fr>
2024-12-13 14:50:02 +0100

    debut js

--- src/js/script.js
+++ src/js/script.js
@@ -12,4 +12,27 @@ if (toggle && nav) {
     });
 }
 
+const carousel = document.querySelector(".carrousel");
+const prevButton = document.querySelector(".arrow__carrousel--left");
+const nextButton = document.querySelector(".arrow__carrousel--right");
 
+if (carousel && prevButton && nextButton) {
+    // Calculer la largeur d'un élément avec l'espace entre eux
+    const itemWidth = document.querySelector(".carrousel__img--film").offsetWidth + 24;
+
+    // Flèche gauche - Défilement vers la gauche
+    prevButton.addEventListener("click", () => {
+        carousel.scrollBy({
+            left: -itemWidth, // Défiler vers la gauche
+            behavior: "smooth", // Animation douce
+        });
+    });
+
+    // Flèche droite - Défilement vers la droite
+    nextButton.addEventListener("click", () => {
+        carousel.scrollBy({
+            left: itemWidth, // Défiler vers la droite
+            behavior: "smooth", // Animation douce
+        });
+    });
+}
